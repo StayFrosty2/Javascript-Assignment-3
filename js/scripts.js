f2b84ee41eada56dbc94fc5d74bb708eedaba61c
Claire <cameron.liam.yon@gmail.com>
2026-08-08 16:05:17 -0400

    setup basics for all pages

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Element References
+
+// Function to dynamically add my name and student id to the document (taken from assignment 2 as it's the same code)
+function createFooter() {
+    let p = document.createElement("p");
+    let studentName = "Cameron Yon-Vachon";
+    let studentId = "200641927";
+    p.textContent = `Page created by: ${studentName} | ${studentId}`;
+    footer.appendChild(p);
+}
