883462878a8c454da9fd5d12cf389300a918f594
Claire <cameron.liam.yon@gmail.com>
2026-08-09 20:56:42 -0400

    changing the objective of the site, included watermark for the api used in the footer

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,6 @@ const p1t1 = document.querySelector("#p1t1");
 const p1t2 = document.querySelector("#p1t2");
 const btn1 = document.querySelector("#searchbtn1");
 
-const p2 = document.querySelector("#p2");
-const p2t1 = document.querySelector("#p2t1");
-const p2t2 = document.querySelector("#p2t2");
-const btn2 = document.querySelector("#searchbtn2");
-
-const p3 = document.querySelector("#p3");
-const p3t1 = document.querySelector("#p3t1");
-const p3t2 = document.querySelector("#p3t2");
-const btn3 = document.querySelector("#searchbtn3");
-
-const p4 = document.querySelector("#p4");
-const p4t1 = document.querySelector("#p4t1");
-const p4t2 = document.querySelector("#p4t2");
-const btn4 = document.querySelector("#searchbtn4");
-
-const p5 = document.querySelector("#p5");
-const p5t1 = document.querySelector("#p5t1");
-const p5t2 = document.querySelector("#p5t2");
-const btn5 = document.querySelector("#searchbtn5");
-
-const p6 = document.querySelector("#p6");
-const p6t1 = document.querySelector("#p6t1");
-const p6t2 = document.querySelector("#p6t2");
-const btn6 = document.querySelector("#searchbtn6");
 
 // Function to dynamically add my name and student id to the document (taken from assignment 2 as it's the same code)
 function createFooter() {
@@ -39,6 +15,16 @@ function createFooter() {
     let studentId = "200641927";
     p.textContent = `Page created by: ${studentName} | ${studentId}`;
     footer.appendChild(p);
+
+    let p2 = document.createElement("p");
+    let a = document.createElement("a");
+
+    p2.textContent = `This site uses PokeAPI, available at: `;
+    a.textContent = `https://pokeapi.co/`;
+    a.href = `https://pokeapi.co/`;
+
+    p2.appendChild(a);
+    footer.appendChild(p2);
 }
 
 // Call the create footer function
